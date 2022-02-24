d1a5c3a9008cc429b8409428f1b736551a6d93b8
vinaykumard81 <97220849+vinaykumard81@users.noreply.github.com>
2022-02-24 10:03:04 +0000

    P3

--- p2final.c
+++ p2final.c
@@ -1,1 +1,27 @@
-
+#include<stdio.h>
+void input_triangle(float*x1,float*y1,float*x2,float*y2,float*x3,float*y3)
+{
+  printf("Enter co-ordinates of the triangle\n");
+  scanf("%f%f%f%f%f%f",x1,y1,x2,y2,x3,y3);
+}
+float is_triangle(float x1, float y1, float x2, float y2,float x3, float y3)
+{
+  float istriangle;
+  istriangle = 0.5*((x1*(y2-y3))+(x2*(y3-y1))+(x3*(y1-y2)));
+  return istriangle;
+}
+void output(float istraingle)
+{
+  if(istraingle==0)
+    printf("The given points does not form a triangle");
+  else
+    printf("The given points forms a triangle");
+}
+int main()
+{
+  float x1,y1,x2,y2,x3,y3,istriangle;
+  input_triangle(&x1,&y1,&x2,&y2,&x3,&y3);
+  istriangle = is_triangle(x1,y1,x2,y2,x3,y3);
+  output(istriangle);
+  return 0;
+}
--- p6final.c
+++ p6final.c
@@ -1,1 +1,36 @@
-
+#include<stdio.h>
+#include<math.h>
+int input_number()
+{
+  int a;
+  printf("enter the number\n");
+  scanf("%d",&a);
+  return a;
+}
+ 
+int is_prime(int n)
+{
+  if (n==0 || n==1)
+  return 1;  
+  for (int i=2;i<sqrt(n);i++)
+  {
+    if((n%i) ==0)
+    return 0;
+  }
+  return 1;
+}
+void output(int n, int is_prime)
+{
+  if (is_prime==1)
+    printf("%d is a prime number\n",n);
+  else 
+    printf("%d is not a prime number\n",n);
+}
+int main()
+{
+  int n,cmp;
+  n=input_number();
+  cmp=is_prime(n);
+  output(n,cmp);
+  return 0;
+}
